5054684b9a4617744046fe6c9adf970420876b9d
md.tousif <md.tousif@blr.cordis>
2024-04-16 20:15:44 +0530

    Initial Commit: Created basic Empty Skeleton Spring Boot project structure

---
+++ src/main/java/com/example/springbootweb3initializrdemoartifact/SpringBootWeb3InitializrDemoArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb3initializrdemoartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb3InitializrDemoArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb3InitializrDemoArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootweb3initializrdemoartifact/SpringBootWeb3InitializrDemoArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb3initializrdemoartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeb3InitializrDemoArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
